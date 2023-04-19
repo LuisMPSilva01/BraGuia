91ab5aa2491e506c0205656365bd91e5c988603a
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2023-04-19 01:34:28 +0100

    discovery searchview

--- app/src/main/java/com/example/braguia/DiscoverFragment.java
+++ app/src/main/java/com/example/braguia/DiscoverFragment.java
@@ -2,36 +2,47 @@ package com.example.braguia;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DiscoverFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+/*
+* Neste momento, este fragmento só imprime o conteudo escrito no centro do ecrã.
+* TODO: Podemos permitir pesquisa por nome, sugerir locais (random?) debaixo da searchview, ...
+* */
 public class DiscoverFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private SearchView searchView;
+    private TextView searchText;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DiscoverFragment() {
-        // Required empty public constructor
-    }
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FrameLayout rootView = (FrameLayout) inflater.inflate(R.layout.fragment_discover, container, false);
+
+        searchText = rootView.findViewById(R.id.search_text);
+
+        searchView = rootView.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchText.setText(query);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return rootView;
     }
 }
