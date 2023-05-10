4599e4c13553fcbe0afe943a359f20e61bb143cc
Gonçalo Pereira <gteixeira.pereira@gmail.com>
2023-05-10 03:13:41 +0100

    Search trails by name

--- app/src/main/java/com/example/braguia/ui/TrailsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/braguia/ui/TrailsRecyclerViewAdapter.java
@@ -1,26 +1,35 @@
 package com.example.braguia.ui;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.braguia.R;
 import com.example.braguia.model.trails.Trail;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TrailsRecyclerViewAdapter extends RecyclerView.Adapter<TrailsRecyclerViewAdapter.ViewHolder> {
+public class TrailsRecyclerViewAdapter extends RecyclerView.Adapter<TrailsRecyclerView> {
 
-    private final List<Trail> mValues;
+    private final List<Trail> originalData;
+
+    private List<Trail> filteredData;
+
     private OnItemClickListener listener;
 
     public TrailsRecyclerViewAdapter(List<Trail> items) {
-        mValues = items;
+        originalData = items;
+        filteredData = new ArrayList<>(items);
     }
 
     public interface OnItemClickListener {
@@ -31,57 +40,53 @@ public class TrailsRecyclerViewAdapter extends RecyclerView.Adapter<TrailsRecycl
         this.listener = listener;
     }
 
+
+    public void filterData(String query) {
+        filteredData.clear();
+        for (Trail data : originalData) {
+            if (data.getTrail_name().toLowerCase().contains(query.toLowerCase())) {
+                filteredData.add(data);
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public void reset(){
+        filteredData = new ArrayList<>(originalData);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public TrailsRecyclerView onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_trail_item, parent, false);
-        return new ViewHolder(view);
+
+        return new TrailsRecyclerView(view);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.trailName.setText(mValues.get(position).getTrail_name());
-        String durationText = mValues.get(position).getTrail_duration() + " minutos";
+    public void onBindViewHolder(final TrailsRecyclerView holder, int position) {
+        holder.mItem = filteredData.get(position);
+        holder.trailName.setText(filteredData.get(position).getTrail_name());
+        String durationText = filteredData.get(position).getTrail_duration() + " minutos";
         holder.duration.setText(durationText);
-        holder.difficulty.setText(mValues.get(position).getTrail_difficulty());
-        Picasso.get().load(mValues.get(position)
+        holder.difficulty.setText(filteredData.get(position).getTrail_difficulty());
+        Picasso.get().load(filteredData.get(position)
                 .getTrail_img().replace("http", "https"))
                 .into(holder.imageView);
 
         // Set click listener for each item
         holder.itemView.setOnClickListener(v -> {
             if (listener != null) {
-                listener.onItemClick(mValues.get(position));
+                listener.onItemClick(originalData.get(position));
             }
+
         });
     }
 
     @Override
-    public int getItemCount() {
-        return mValues.size();
-    }
+    public int getItemCount() {return filteredData.size();}
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public final TextView trailName;
-        public final TextView difficulty;
-        public final TextView duration;
-        public final ImageView imageView;
-        public Trail mItem;
 
-        public ViewHolder(View view) {
-            super(view);
-            mView = view;
-            trailName = view.findViewById(R.id.trailTitle);
-            difficulty = view.findViewById(R.id.trailDifficulty);
-            duration = view.findViewById(R.id.trailTime);
-            imageView = view.findViewById(R.id.cardimage);
-        }
-
-        @Override
-        public String toString() {
-            return super.toString() + trailName+difficulty+duration;
-        }
-    }
 }
