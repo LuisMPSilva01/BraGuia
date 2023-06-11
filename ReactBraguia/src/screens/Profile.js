410a070bfbcfc07beb06c6feb3bd64b7857976ad
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2023-06-11 15:23:34 +0100

    profile done
    TODO: trails history

--- ReactBraguia/src/screens/Profile.js
+++ ReactBraguia/src/screens/Profile.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import BottomBar from '../components/BottomBar';
+import { useSelector } from 'react-redux';
 
 const Profile = ({ navigation }) => {
   const handleTrailsHistory = () => {
     navigation.navigate('Trails History');
   };
+
+  const userMetaData = useSelector((state) => state.user.username);
+  const {
+    username,
+    email,
+    date_joined,
+    user_type,
+  } = userMetaData;
+
+  const formattedDateJoined = new Date(date_joined).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
   return (
     <View style={styles.container}>
@@ -14,20 +29,30 @@ const Profile = ({ navigation }) => {
         <View style={styles.profileContainer}>
           <View style={styles.backgroundContainer}>
             <Ionicons name="md-person-circle" size={150} color="#FFFFFF" />
-            <Text style={styles.username}>Cesário Verde</Text>
+            <Text style={styles.username}>{username}</Text>
             <Text style={styles.infoTitle}>User type</Text>
-            <Text style={styles.userType}>Premium</Text>
+            <Text style={styles.userType}>{user_type}</Text>
           </View>
         </View>
 
         <View style={styles.detailContainer}>
           <View style={styles.detailRow}>
             <Ionicons name="md-person-outline" size={36} color="#000000" />
-            <Text style={styles.detailText}>José Joaquim Cesário Verde</Text>
+            <Text style={styles.detailText}>{username}</Text>
           </View>
           <View style={styles.detailRow}>
             <Ionicons name="md-mail" size={36} color="#000000" />
-            <Text style={styles.detailText}>cesarioVerde@gmail.com</Text>
+            {email ? (
+              <Text style={styles.detailText}>{email}</Text>
+            ) : (
+              <Text style={styles.detailText}>No email available</Text>
+            )}
+          </View>
+          <View style={styles.detailRow}>
+            <Ionicons name="md-calendar" size={36} color="#000000" />
+            <Text style={styles.detailText}>
+              Member since {formattedDateJoined}
+            </Text>
           </View>
         </View>
 
@@ -72,6 +97,8 @@ const styles = StyleSheet.create({
   infoTitle: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#000000',
+    marginTop: 10,
   },
   userType: {
     color: '#FFFFFF',
@@ -89,13 +116,14 @@ const styles = StyleSheet.create({
   detailText: {
     paddingLeft: 20,
     fontWeight: 'bold',
+    fontSize: 16,
+    color: '#000000',
   },
   button: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FF0008',
-    marginTop: 15,
     paddingHorizontal: 10,
     paddingVertical: 10,
     borderRadius: 10,
